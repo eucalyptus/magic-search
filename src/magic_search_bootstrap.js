9a0ae272385e0f653db447a0ed27cf77ddf1bae2
Randy Bertram <rbertram@us.ibm.com>
2015-02-16 16:33:46 -0500

    Make MS Bootstrap open/close menus when should
    Uses show/hideMenu override to enforce the magic_search.js
    logic about menus. For example, the empty menu no longer
    appears for an empty list of options. Also includes a hack
    to $watch for changes in the input JSON, so that the
    menus can change on the fly.

--- src/magic_search_bootstrap.js
+++ src/magic_search_bootstrap.js
@@ -6,8 +6,24 @@ angular.module('MagicSearch', ['ui.bootstrap'])
                 // showMenu and hideMenu depend on foundation's dropdown. They need
                 // to be modified to work with another dropdown implemenation.
                 // For bootstrap, they are not needed at all.
-                $scope.showMenu = function() {};
-                $scope.hideMenu = function() {};
+                $scope.showMenu = function() {
+                    $scope.isMenuOpen = true;
+                };
+                $scope.hideMenu = function() {
+                    $scope.isMenuOpen = false;
+                };
+                $scope.isMenuOpen = false;
+
+                // remove the following when magic_search.js handles changing the facets/options
+                $scope.$watch('facets_json', function(newVal, oldVal) {
+                    if (newVal === oldVal) {
+                        return;
+                    }
+                    $scope.facetsJson = $scope.facets_json.replace(/__apos__/g, "\'").replace(/__dquote__/g, '\\"').replace(/__bslash__/g, "\\");
+                    $scope.facetsObj = JSON.parse($scope.facetsJson);
+                    $scope.resetState();
+                });
+
             }
         };
     });
