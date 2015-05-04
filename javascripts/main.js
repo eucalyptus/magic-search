5a6f616d874b61ff6b9d4bb46110b641df626bd6
dkavanagh <dkavanagh@gmail.com>
2015-05-04 11:59:49 -0400

    updated demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 
 angular.module('Demo', ['MagicSearch'])
-    .controller('DemoCtrl', function($scope) {
+    .controller('DemoCtrl', function($scope, $timeout) {
         $scope.data_set = [
             {'name':'cookies', 'type':'baked'},
             {'name':'cake', 'type':'baked'},
@@ -10,6 +10,7 @@ angular.module('Demo', ['MagicSearch'])
         ];
         $scope.filtered_data = [];
         $scope.init = function() {
+            $scope.faceted_data = $scope.data_set;
             $scope.filtered_data = $scope.data_set;
             // since we're not getting data from the server, do a client-filter based on URL
             var query = window.location.href;
@@ -17,6 +18,9 @@ angular.module('Demo', ['MagicSearch'])
                 query = query.split("?")[1];
                 $scope.$emit('searchUpdated', query);
             }
+            $timeout(function() {
+                $(document).foundation('dropdown', 'reflow');
+            }, 500);
         };
         $scope.$on('searchUpdated', function($event, query) {
             // update url
@@ -30,7 +34,8 @@ angular.module('Demo', ['MagicSearch'])
             window.history.pushState(query, "", url);
             // normally, you would use an XHR call to get new "data_set", but we'll filter client-side for the demo
             var facets = query.split('&');
-            $scope.filtered_data = $scope.data_set.filter(function(item) {
+            $scope.faceted_data = $scope.data_set.filter(function(item) {
+                if (facets.length === 1 && facets[0].length === 0) return item;
                 for (var i=0; i<facets.length; i++) {
                     var facet = facets[i].split('=');
                     var val = item.hasOwnProperty(facet[0]) && item[facet[0]];
@@ -39,10 +44,11 @@ angular.module('Demo', ['MagicSearch'])
                     }
                 }
             });
-            $scope.$apply();
+            $scope.filtered_data = $scope.faceted_data.slice();
+            $timeout(function () { $scope.$apply() });
         });
         $scope.$on('textSearch', function($event, text, filter_keys) {
-            $scope.filtered_data = $scope.data_set.filter(function(item) {
+            $scope.filtered_data = $scope.faceted_data.filter(function(item) {
                 for (var i=0; i<filter_keys.length; i++) {
                     var prop = filter_keys[i];
                     var val = item.hasOwnProperty(prop) && item[prop];
@@ -51,7 +57,7 @@ angular.module('Demo', ['MagicSearch'])
                     }
                 }
             });
-            $scope.$apply();
+            $timeout(function () { $scope.$apply() });
         });
     })
 ;
